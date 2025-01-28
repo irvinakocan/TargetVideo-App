4d90150e715bfb5bff57d06f75efd2516b1ba077
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2025-01-28 17:34:59 +0100

    Create Constants file

---
+++ TargetVideo Task/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  TargetVideo Task
+//
+//  Created by Macbook Air 2017 on 28. 1. 2025..
+//
+
+import Foundation
+
+let YOUTUBE_API = "https://www.googleapis.com/youtube/v3"
+let GET_ENDPOINT = "/videos"
+let API_KEY = "AIzaSyDH9SiV5W4JuUBtKVkpOSxxrUXeAPVdvVA"
+
