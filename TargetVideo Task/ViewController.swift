523519cd29a6e4a9af2add9bbbd11786ea07daeb
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2025-01-28 14:13:19 +0100

    Initial Commit

---
+++ TargetVideo Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TargetVideo Task
+//
+//  Created by Macbook Air 2017 on 28. 1. 2025..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
