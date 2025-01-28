a104088cefe36d7c004a71fac698d321d36c7fbd
irvinakocan <44883640+irvinakocan@users.noreply.github.com>
2025-01-28 15:15:52 +0100

    Set Home Page

---
+++ TargetVideo Task/HomeViewController.swift
@@ -0,0 +1,51 @@
+//
+//  HomeViewController.swift
+//  TargetVideo Task
+//
+//  Created by Macbook Air 2017 on 28. 1. 2025..
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        return tableView
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+        title = "Home page"
+        
+        setTableView()
+    }
+    
+    private func setTableView() {
+        view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        tableView.translatesAutoresizingMaskIntoConstraints = true
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
